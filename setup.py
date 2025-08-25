860f28cc32ae528873e61515448324a7e5b9ea7f
Mstsec <mailspammer444@gmail.com>
2025-08-25 18:32:33 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ setup(
     name='whomst',
     version='0.1.0',
     description='Alan adı ve IP adresi sorgulamak için Python ile yazılmış bir WHOIS aracı.',
-    author='mstsec',
+    author='mstsecurity',
     author_email='mailspammer444@gmail.com',
-    url='https://github.com/mstsec/whomst',
+    url='https://github.com/mstsecurity/whomst',
     packages=find_packages(),
-    install_requires=[
-    ],
+    install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
@@ -18,4 +17,9 @@ setup(
         'Topic :: System :: Networking'
     ],
     python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'whomst = whomst.whomst:main',
+        ],
+    },
 )
